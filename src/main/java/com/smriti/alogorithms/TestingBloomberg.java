8a97b7616281b441bc870a9a8a7bddb881fe0674
smritiraj22 <mail.smritiraj@gmail.com>
2017-04-10 11:24:18 -0500

    Update TestingBloomberg.java

--- src/main/java/com/smriti/alogorithms/TestingBloomberg.java
+++ src/main/java/com/smriti/alogorithms/TestingBloomberg.java
@@ -1,47 +1,47 @@
 package com.smriti.alogorithms;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class TestingBloomberg {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String st1 = "this is a String";
+		String st2 = "this aa ab cc";
+		String[] a = st1.split(" ");
+		String[] b = st2.split(" ");
+		commonSubstring(a, b);
 
 	}
-	
-	  static void commonSubstring(String[] a, String[] b) {
 
-		  int len = a.length;
-	        for(int i =0; i <len; i++){
-	            String fromA = a[i];
-	            String fromB = b[i];
-	            List stringSetA = Arrays.asList(fromA.toCharArray());
-	            List stringSetB = Arrays.asList(fromB.toCharArray());
-	            int lenA = stringSetA.size();
-	            int lenB = stringSetB.size();
-	            if(lenA>lenB){
-	                innerA: for(int j=0; j<lenB;j++){
-	                    if(stringSetA.contains(stringSetB.get(i))){
-	                        System.out.print("Yes");
-	                        break innerA;
-	                    }
-	                }
-	                 System.out.print("No");
-	            }
-	            else{
-	                 innerB: for(int j=0; j<lenA;j++){
-	                    if(stringSetB.contains(stringSetA.get(i))){
-	                        System.out.print("Yes");
-	                        break innerB;
-	                    }
-	                }
-	                System.out.print("Yes");
-	            }
-	            
-	        }
-	    }
+	static void commonSubstring(String[] a, String[] b) {
 
-	  
+		int len = a.length;
+
+		outer:for (int i = 0; i < len; i++) {
+
+			String fromA = a[i];
+			String fromB = b[i];
+
+			int lenA = fromA.length();
+			int lenB = fromB.length();
+
+			if (lenA >= lenB) {
+				for (int j = 0; j < lenB; j++) {
+					if (fromA.indexOf(fromB.charAt(j)) == 0) {
+						System.out.println("Yes");
+						continue outer;
+					}
+				}
+				System.out.println("No");
+			} else {
+				 for (int j = 0; j < lenA; j++) {
+					if (fromB.indexOf(fromA.charAt(j)) == 0) {
+						System.out.println("Yes");
+						continue outer;
+					}
+				}
+				System.out.println("No");
+			}
+
+		}
+	}
 
 }
